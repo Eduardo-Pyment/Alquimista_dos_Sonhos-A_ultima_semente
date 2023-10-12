7c1fe1b3b882f0d73627a8dbcce40f5319a6bfed
GlauberMargem <126527963+GlauberMargem@users.noreply.github.com>
2023-10-12 18:09:39 -0300

    Atualização_mecanica do h2o

---
+++ Scripts/hidrogenios.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var hidro :=1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$animation.play("idle")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	$animation.play("collected")
+	#remover a colisão dupla // espera a colisão acabar para computar a coleta
+	await $collision.call_deferred("queue_free") 
+	Globais.hidrogenio += hidro
+	print(Globais.hidrogenio)
+func _on_animated_sprite_2d_animation_finished():
+	queue_free()
---
+++ Scripts/maquina_agua.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var inside = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_up") and inside == true:
+		if Globais.hidrogenio >= 2 and Globais.oxigenio >= 1:
+			Globais.hidrogenio -= 2
+			Globais.oxigenio -= 1
+			Globais.agua +=1
+			print("agua = ",Globais.agua)
+
+
+func _on_body_entered(body):
+	inside = true
+
+
+func _on_body_exited(body):
+	inside = false
