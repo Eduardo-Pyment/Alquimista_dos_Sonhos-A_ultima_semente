e0688b45e91b0da15a146babdc63f6480a7d2d8c
Bruno0M <brunomedeirosr09@gmail.com>
2023-10-23 21:34:03 -0300

    refatoração do save and load e corrigido bug do fullscreen

--- Menus/MenuPrincipal/MenuOpcoes.gd
+++ Menus/MenuPrincipal/MenuOpcoes.gd
@@ -1,9 +1,8 @@
 extends Control
 
 @onready var music_slider: HSlider = %MusicaSlider
-@onready var full_screen = %FullScreen
+@onready var full_screen: CheckButton = %FullScreen
 
-var SETTINGS_FILE_PATH = "user://options.cfg"
 var config = ConfigFile.new()
 
 func _ready():
@@ -13,36 +12,30 @@ func _on_voltar_pressed():
 	save_opcoes()
 	get_tree().change_scene_to_file("res://Menus/MenuPrincipal/menu_principal.tscn")
 
-func load_opcoes():
-	var error = config.load(SETTINGS_FILE_PATH)
-	if error == OK:
-		music_slider.value = config.get_value("SETTINGS_MENU_SECTION", "MUSIC_SLIDER_VALUE", 0.5)
-		
-		#Essa linha não ta funcionando:
-		#full_screen.pressed = config.get_value("SETTINGS_MENU_SECTION", "FULLSCREEN_VALUE")
-	else:
-		print("Erro ao carregar configurações.")
-
 func save_opcoes():
-	config.set_value("SETTINGS_MENU_SECTION", "MUSIC_SLIDER_VALUE", music_slider.value)
+	config.set_value(OptionsConstants.section_name, OptionsConstants.music_volume_key_name, music_slider.value)
+	config.set_value(OptionsConstants.section_name, OptionsConstants.fullscreen_key_name, full_screen.button_pressed)
 	
-	#Essa linha complementa a de cima e não está funcionando
-	#config.set_value("SETTINGS_MENU_SECTION", "FULLSCREEN_VALUE", full_screen.pressed)
-	var error = config.save(SETTINGS_FILE_PATH)
-	if error == OK:
-		print("Configurações salvas com sucesso.")
-	else:
-		print("Erro ao salvar configurações.")
+	config.save(OptionsConstants.config_file_name)
+
+func load_opcoes():
+	var err = config.load(OptionsConstants.config_file_name)
+	
+	var music_volum = config.get_value(OptionsConstants.section_name, OptionsConstants.music_volume_key_name, 1)
+	var fullscreen_button = config.get_value(OptionsConstants.section_name, OptionsConstants.fullscreen_key_name, false)
+	
+	music_slider.value = music_volum
+	full_screen.button_pressed = fullscreen_button
 
 func _on_musica_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value / 10.0))
 
-func _on_full_screen_toggled(button_pressed: bool):
-	if button_pressed == true:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+func _on_full_screen_toggled(button_pressed):
 	if button_pressed:
-		print("Botão Full Screen pressionado")
+		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
-		print("Tá não em")
+		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
---
+++ Menus/MenuPrincipal/OpcoesConstants.gd
@@ -0,0 +1,8 @@
+extends Node
+class_name OptionsConstants
+
+const config_file_name = "user://options.cfg"
+
+const section_name = "Options" 
+const music_volume_key_name = "music_volume" 
+const fullscreen_key_name = "fullscreen"
