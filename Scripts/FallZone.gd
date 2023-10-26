72f72f24fe7ef872825ec6eb0d5d6e97b8b3eed1
GlauberMargem <126527963+GlauberMargem@users.noreply.github.com>
2023-10-26 18:04:15 -0300

    Particulas de vapor
    Adicionei particulas de vapor, o objeto do fogo é destruído, caso o player tenha água no inventário

--- Scripts/FallZone.gd
+++ Scripts/FallZone.gd
@@ -1,14 +1,19 @@
 extends Area2D
 @onready var collision_shape_2d = $CollisionShape2D
 @onready var gpu_particles_2d = $GPUParticles2D
-
+@onready var vapor = $"../Vapor"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-		
+	vapor.emitting = false
 
 
 func _on_body_entered(body):
 	if body.name == 'Player' && body.has_method('take_damage'):
 		body.take_damage(Vector2(0, -130))
+	if Globais.agua >= 1:
+		await queue_free()
+		particulas()
+		
+func particulas():
+	vapor.emitting = true
