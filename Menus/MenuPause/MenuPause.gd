6e19e0bf42cd039149dd7c1d6c7c5d5cf82f476f
Bruno0M <brunomedeirosr09@gmail.com>
2023-11-03 22:54:54 -0300

    Add func reload game

--- Menus/MenuPause/MenuPause.gd
+++ Menus/MenuPause/MenuPause.gd
@@ -1,10 +1,7 @@
 extends CanvasLayer
 
-
 func _ready():
 	visible = false
-	#connect("pressed", self, "_on_button_pressed")
-
 
 func _process(delta):
 	pass
@@ -13,14 +10,13 @@ func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
 		visible = true
 		get_tree().paused = true
+	
+func reload_game():
+	get_tree().reload_current_scene()
 
 func _on_reiniciar_pressed():
-	var game_scene = get_tree().get_root().get_node("GameScene")
-	if game_scene:
-		game_scene.queue_free()
-		var new_game_scene = preload("res://Fases/fase_1.tscn")
-		var instance = new_game_scene.instance()
-		get_tree().get_root().add_child(instance)
+	reload_game()
+	get_tree().paused = false
 
 func _on_opcoes_pressed():
 	pass # Replace with function body.
