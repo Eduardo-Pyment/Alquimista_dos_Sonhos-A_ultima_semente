f80e705dec1816631597eebbc9cb0c9dc615308d
Eduardo Pimenta <edupymota@protonmail.com>
2023-11-05 10:50:44 -0300

    Portal adicionado em todas as fases

--- Scripts/Zona_fogo_generica.gd
+++ Scripts/Zona_fogo_generica.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
 func _on_body_entered(body):
+	Globais.agua = 0
 	if body.name == 'Player' && body.has_method('take_damage'):
-		body.take_damage(Vector2(0, -130))
+		body.take_damage(Vector2(0, -130))	
