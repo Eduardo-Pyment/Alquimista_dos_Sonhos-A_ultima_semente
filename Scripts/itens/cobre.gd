2e0e384cf41ed645b6e69e4fd99b0fcdd16ca4dd
MatheusLuv <matheusmenezesdacruz9@gmail.com>
2023-11-05 21:08:26 -0300

    crie os atomos e ajustei as camadas

---
+++ Scripts/itens/cobre.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var oxigen := 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$animation.play("idle")
+
+func _on_body_entered(_body):
+	$animation.play("collected")
+	#remover a colisão dupla // espera a colisão acabar para computar a coleta
+	Globais.oxigenio += oxigen
+	print(Globais.oxigenio)
+	call_deferred("spawn_atomo")
+	await $collision.call_deferred("queue_free") 
+
+func spawn_atomo():
+	var hidrogenio_flutuante = load("res://Scenes/Itens/Cu_flutuante.tscn")
+	var novo_atomo = hidrogenio_flutuante.instantiate()
+	get_node("/root/Fase_2/cobre").add_child(novo_atomo)	
+	
+func _on_animation_animation_finished():	
+	pass
