7cd8b3dc28cb5a22676c17ca5e4b3b50d31742db
LisandroVA2002 <llisandrovieira@gmail.com>
2023-11-07 16:33:29 -0300

    Correcao da maquina

--- Scripts/Globais.gd
+++ Scripts/Globais.gd
@@ -7,3 +7,7 @@ var player_life := 3
 var sodio := 0
 var cloro := 0
 var base := 0
+var cobre := 0
+var zinco := 0
+var prata := 0
+var solido := 0
--- Scripts/itens/prata.gd
+++ Scripts/itens/prata.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var oxigen := 1
+var silver := 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,14 +9,14 @@ func _ready():
 func _on_body_entered(_body):
 	$animation.play("collected")
 	#remover a colisão dupla // espera a colisão acabar para computar a coleta
-	Globais.oxigenio += oxigen
-	print(Globais.oxigenio)
+	Globais.prata += silver
+	print(Globais.prata)
 	call_deferred("spawn_atomo")
 	await $collision.call_deferred("queue_free") 
 
 func spawn_atomo():
-	var hidrogenio_flutuante = load("res://Scenes/Itens/Ag_flutuante.tscn")
-	var novo_atomo = hidrogenio_flutuante.instantiate()
+	var ag_flutuante = load("res://Scenes/Itens/Ag_flutuante.tscn")
+	var novo_atomo = ag_flutuante.instantiate()
 	get_node("/root/Fase_2/prata").add_child(novo_atomo)	
 	
 func _on_animation_animation_finished():	
